6a47926eb61b2210223af576e400136ca4bac2b1
John Bayly <john.bayly@tipstrade.net>
2018-05-08 14:17:41 +0100

    Fix indents

--- cauldron/cauldron_core.h
+++ cauldron/cauldron_core.h
@@ -6,8 +6,7 @@ typedef enum {
 	CAULDRON_MODE_FAN = 0x1,
 	CAULDRON_MODE_FIRE = 0x2,
 	CAULDRON_MODE_GLOW = 0x4,
-        CAULDRON_MODE_ALL = 0xff
-  
+  CAULDRON_MODE_ALL = 0xff
 } cauldron_mode_e;
 
 // The configuration that is send over the RF24 link
